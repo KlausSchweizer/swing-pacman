fbdb54b8db03eb1ef4051fa2ec2c6436280ec892
Klaus Essig Schweizer <klaus@spmodelismo.com.br>
2025-09-28 16:59:59 -0300

    Teste.java identado e mapa.txt atualizado

--- pacman/src/main/java/mapa/Teste.java
+++ pacman/src/main/java/mapa/Teste.java
@@ -12,16 +12,15 @@ import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import mapa.TestePanel;
 
-public class Teste
-extends JFrame {
+public class Teste extends JFrame {
+
     public Teste() {
         this.initComponents();
         TestePanel painelPrincipal = new TestePanel();
         this.add(painelPrincipal);
         this.getContentPane().setLayout(new BorderLayout());
-        this.getContentPane().add((Component)painelPrincipal, "Center");
+        this.getContentPane().add((Component) painelPrincipal, "Center");
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
@@ -39,24 +38,22 @@ extends JFrame {
     public static void main(String[] args) {
         try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if (!"Nimbus".equals(info.getName())) continue;
+                if (!"Windows".equals(info.getName())) {
+                    continue;
+                }
                 UIManager.setLookAndFeel(info.getClassName());
                 break;
             }
-        }
-        catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
             Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch (InstantiationException ex) {
-            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (IllegalAccessException ex) {
-            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        EventQueue.invokeLater(new Runnable(){
+        EventQueue.invokeLater(new Runnable() {
 
             @Override
             public void run() {
